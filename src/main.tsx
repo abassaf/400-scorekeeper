c8835cf3e21972cc4514d5158464322c533f4863
abassaf <31185306+abassaf@users.noreply.github.com>
2026-04-22 23:27:00 +1000

    feat: add ThemeContext with light/dark/system mode and localStorage persistence

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import { ThemeProvider } from './context/ThemeContext.tsx'
 import './index.css'
 
 const root = document.getElementById('root')!
 
 ReactDOM.createRoot(root).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 )
